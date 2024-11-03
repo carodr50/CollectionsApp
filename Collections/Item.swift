069bc048b92868176b7fe0a17a668e5dbb76269b
carodr50 <carodr50@asu.edu>
2024-11-03 13:16:26 -0700

    Edited generated default model to better suit my needs for this application

--- Collections/Item.swift
+++ Collections/Item.swift
@@ -9,10 +9,20 @@ import Foundation
 import SwiftData
 
 @Model
-final class Item {
+final class Item : Identifiable {
+    var id: UUID
+    var pName: String
+    var pCost: Decimal
+    var pAmount: Int
+    var pImage: String
     var timestamp: Date
     
-    init(timestamp: Date) {
+    init(timestamp: Date, pName: String, pCost: Decimal, pAmount: Int, pImage: String) {
+        self.id = UUID()
         self.timestamp = timestamp
+        self.pName = pName
+        self.pCost = pCost
+        self.pAmount = pAmount
+        self.pImage = pImage
     }
 }
